2dfe680af2d646839a786f30bd269754cdaf8d11
Saeful Ardi <saeful.4rdi@gmail.com>
2023-05-08 23:16:37 +0700

    Menambahkan pagination

--- obat/data.php
+++ obat/data.php
@@ -10,7 +10,7 @@
             
             <div class="col-lg-4">
                 <div class="">
-                    <form action="" class="form-inline" method="post">
+                    <form action="" class="form-inline" method="POST">
                         <div class="input-group">
                             <input type="text" name="pencarian" class="form-control" placeholder="Pencarian">
                             <button class="btn btn-primary"><i class="bi bi-search"></i></button>
@@ -20,7 +20,6 @@
             </div>
             <div class="col-lg-8">
                 <h4>
-                    
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         <a href="" class="btn btn-success btn-x5"><i class="bi bi-arrow-clockwise"></i></a>
                         <a href="" class="btn btn-success btn-x5"><i class="bi bi-plus-lg"></i> Tambah Data</a>
@@ -40,6 +39,31 @@
                         </thead>
                         <tbody>
                             <?php
+                            $batas = 3;
+                            $hal = @$_GET['hal'];
+                            if(empty($hal)) {
+                                $posisi = 0;
+                                $hal = 1;
+                            } else {
+                                $posisi = ($hal - 1) * $batas;
+                            } 
+                            if($_SERVER['REQUEST_METHOD'] == "POST") {
+                                $pencarian = trim(mysqli_real_escape_string($conn, $_POST['pencarian']));
+                                if($pencarian != '' ) {
+                                    $sql = "SELECT * FROM obat WHERE nama_obat LIKE '%$pencarian%'";
+                                    $query = $sql;
+                                    $queryJml = $sql;
+                                } else {
+                                    $query = "SELECT * FROM obat LIMIT $posisi, $batas";
+                                    $queryJml = "SELECT * FROM obat";
+                                    $no = $posisi + 1;
+                                }
+                            } else {
+                                $query = "SELECT * FROM obat LIMIT $posisi, $batas";
+                                $queryJml = "SELECT * FROM obat";
+                                $no = $posisi + 1;
+                            }
+
                             $no = 1;
                             $sql_obat = mysqli_query($conn, "SELECT * FROM obat") or die (mysqli_error($conn));
                             if (mysqli_num_rows($sql_obat) > 0) {
@@ -49,7 +73,7 @@
                                     <td><?= $data['nama_obat']?></td>
                                     <td><?= $data['ket_obat']?></td>
                                     <td>
-                                        <a href="">Edit</a> | <a href="">Hapus</a>
+                                        <a href=""><i class="bi bi-pencil-square"></i></a> | <a href=""><i class="bi bi-trash"></i></a>
                                     </td>
                                 </tr>    
                                 <?php 
@@ -65,11 +89,38 @@
                         </tbody>
                     </table>
                 </div>
+                <?php
+                    if($_POST['pencarian'] == '') { ?>
+                        <div style="float:left;">
+                            <?php
+                            $jml = mysqli_num_rows(mysqli_query($conn, $queryJml));
+                            echo "Jumlah Data: <b>$jml</b>";
+                            ?>
+                        </div>
+                        <div style="float: right;">
+                            <ul class="pagination pagination-sm" style="margin: 0;">
+                                <?php
+                                $jml_hal = ceil($jml / $batas);
+                                for ($i=1; $i <= $jml_hal; $i++) { 
+                                    if ($i != $hal) {
+                                        echo "<li><a href=\"?hal=$i\">$i</a></li>";
+                                    } else {
+                                        echo "<li><a href=\"active\">$i</a></li>";
+                                    }
+                                }
+                                ?>
+                            </ul>
+                        </div>
+                    <?php
+                    } else {
+                        echo "<div style=\"float:left\">";
+                        $jml = mysqli_num_rows(mysqli_query($conn, $queryJml));
+                        echo "Data hasil pencarian : <b>$jml</b>";
+                        echo "</div>";
+                    }
+                    ?>
             </div>
-            
         </div>
-            
-        
     </div>
 </div>
 
